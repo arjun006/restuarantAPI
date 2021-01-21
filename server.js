727ad65b72240f432365e659bae4b52233ad37e8
arjun006 <arjun.devakumar66@gmail.com>
2021-01-21 14:56:36 -0500

    status codes updated

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+*  WEB422 – Assignment 1
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: Arjun Devakumar Student ID: 159076199 Date: January 20, 2021
+*  Heroku Link: https://obscure-mesa-81981.herokuapp.com/
+*
+********************************************************************************/ 
+
 const express = require('express');
 const router = express.Router();
 const path = require("path");
@@ -27,20 +38,23 @@ db.initialize().then(()=>{
 
 //API Routes
 app.get('/',function(req,res){
-    res.json({message:"API Listening..."});
+    res.status(200).json({message:"API Listening..."})
+    .catch((err)=>{
+        res.status(500).json({message:`Server Error: ${err}`})
+    })
 });
 app.get('/api/restaurants',(req,res)=>{
     db.getAllRestaurants(req.query.page, req.query.perPage, req.query.borough)
     .then((restaurants)=>{
-        res.json(restaurants);
+        res.status(200).json(restaurants);
     }).catch((err)=>{
-        res.status(404).json({message:"Error getting restaurants"});
+        res.status(500).json({message:"Error getting restaurants"});
     })
 });
 app.get('/api/restaurants/:id',(req,res)=>{
     db.getRestaurantById(req.params.id)
     .then((restaurant)=>{
-        res.json(restaurant);
+        res.status(200).json(restaurant);
     })
     .catch((err)=>{
         res.status(404).json({message:`Could not find restaurant with id: ${req.params.id}`})
@@ -52,17 +66,17 @@ app.post("/api/restaurants", (req, res) => {
     .then(res.status(201)
     .json({message:"Successfully added restaurant"}))
     .catch((err)=>{
-        res.status(400).json({message:"Error adding restaurant"})
+        res.status(500).json({message:"Error adding restaurant"})
     })
   });
 
   app.put('/api/restaurants/:id', (req,res)=>{
       db.updateRestaurantById(req.body, req.params.id)
       .then((updateRes)=>{
-          res.json(updateRes)
+          res.status(201).json(updateRes)
       })
       .catch((err)=>{
-          res.status(500).json({message:`Error updating restaurant ${err}`})
+          res.status(500).json({message:`Error updating restaurant: ${err}`})
       })
   });
   app.delete("/api/restaurants/:id",(req,res)=>{
@@ -71,12 +85,6 @@ app.post("/api/restaurants", (req, res) => {
           res.status(200).json({message:`Restaurant successfully deleted: ${deleteRes}`})
       })
       .catch((err)=>{
-          res.status(500).json({message:"Error deleting"})
+          res.status(500).json({message:`Error deleting: ${err}`})
       })
   })
-
-
-//Server Listening
-// app.listen(PORT,function(){
-//     console.log(`🌎 ==> Server listening now on port ${PORT}!`);
-// });
